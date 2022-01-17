6818f44dcd1e57523f016c5230524f7cdd57ff83
dimababenko <dima.babenko@ukr.net>
2022-01-17 19:43:19 +0700

    Page transition added

--- test_main_page.py
+++ test_main_page.py
@@ -14,8 +14,10 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-#def test (browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-#    page = LoginPage(browser, link)
-#    page.open()
-#   page.should_be_login_page()
+def test_quest_can_go_to_login(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
